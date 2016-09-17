9d143df1b036f68c3c36e8d74b3ff38062ab3d13
Yen <yenchieh86@hotmail.com>
2016-09-17 00:05:46 -0700

    cp5-assignment: finished

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,13 @@
+(function(){
+    function AlbumCtrl{
+        this.albumData = [];
+        for( var i = 0; i < albumData.length; i++){
+            this.albumDate.push(angular.copy(albumPicasso))
+        }
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', AlbumCtrl);
+    
+})();
