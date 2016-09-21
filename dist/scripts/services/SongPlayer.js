0cf712f7db28df03b396cfe7722e0e59f786807f
Yen <yenchieh86@hotmail.com>
2016-09-21 00:28:20 -0700

    Cp7-assignment: Finished

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,6 +1,16 @@
 (function(){
     function SongPlayer(){
+        
+        /**
+        * @desc a variable
+        * @type {Object}
+        */
         var SongPlayer = {};
+        
+        /**
+        * @desc current playing song
+        * @type {Object}
+        */
         var currentSong = null;
         
         /**
@@ -28,14 +38,23 @@
             currentSong = song;
         };
         
+        /**
+        * @function playSong
+        * @desc Play currentBuzzObject, and set song.playing to true
+        * @param {Object} song
+        */
+        var playSong = function(song){
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+        
         SongPlayer.play = function(song){
             if(currentSong !== song){
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if(currentSong === song){
                 if(currentBuzzObject.isPaused()){
-                    currentBuzzObject.play();
+                    playSong(song);
                     
                 }
             }
